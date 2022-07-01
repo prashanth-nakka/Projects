06200ed7c92c002f62805916196015352bc168cb
Prashanth <prashanth041997@outlook.com>
2022-07-01 13:57:18 +0530

    added patterns

--- Python_Classes/Patterns/main.py
+++ Python_Classes/Patterns/main.py
@@ -35,6 +35,10 @@ for i in range(1, n + 1):
 1  2  3  4  5  
 1  2  3  4  5
 '''
+
+# i is Outer loop to print no. of rows
+# j is inner loop to print no. of columns
+
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         print(j, " ", end="")
@@ -80,4 +84,45 @@ for i in range(1, n + 1):
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         print((n + 1 - i), " ", end="")
+    print()
+
+
+#pattern - 7
+'''
+5  4  3  2  1  
+5  4  3  2  1  
+5  4  3  2  1  
+5  4  3  2  1  
+5  4  3  2  1
+'''
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print((n + 1 - j), " ", end="")
+    print()
+
+# pattern - 8
+'''
+E  E  E  E  E  
+D  D  D  D  D  
+C  C  C  C  C  
+B  B  B  B  B  
+A  A  A  A  A
+'''
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print(chr(65 + n - i), " ", end="")
+    print()
+
+
+# pattern - 9
+'''
+E  D  C  B  A  
+E  D  C  B  A  
+E  D  C  B  A  
+E  D  C  B  A  
+E  D  C  B  A
+'''
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print(chr(65 + n - j), " ", end="")
     print()
